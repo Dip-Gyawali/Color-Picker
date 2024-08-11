c2d5a51ea309489235074a7cf0e19fdca9a77eab
Hiten <hitengamer11@gmail.com>
2024-08-11 20:47:02 +0545

    css added , all completed

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -7,12 +7,12 @@ function ColorPicker(){
         setColor(e.target.value);
     }
    return(
-    <div className="container">
+    <div className="color">
         <h1>Color Picker</h1>
-        <div className="color-body">
+        <div className="color-body" style={{backgroundColor:color}}>
             <p>Color :{color}</p>
         </div>
-        <label>
+        <label className="label">
             Pick Color
             <input type="color" value={color} onChange={changeColor}/>
         </label>
